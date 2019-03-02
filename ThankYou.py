91d12d1c2ff784709c57f40f83fabd69cc7526ba
zpl <zpl@hotmail.com>
2019-03-02 19:16:18 +0800

    致谢

---
+++ ThankYou.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+#ThankYou.py 感谢
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+my_name="zhaopeilin"
+my_age=32
+#raw=raw_input("你的名字是什么？")
+#your_name=raw.encode("utf-8")
+#TODO 在centos的终端下，输入中文字句有编码问题，程序不能正常运行
+your_name=input("你的名字是什么？")
+your_age=input("How old are you？")
+print("我的名字是",my_name,",我有",my_age,"岁")
+print("你叫",your_name,",你有",your_age,"岁")
+print("感谢你来和我一起学习python编程课程，",your_name,"!")

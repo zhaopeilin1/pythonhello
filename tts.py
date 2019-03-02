09bbd3ef91e5b29848ccb064e636abcae8cb718b
zpl <zpl@hotmail.com>
2019-03-02 20:37:51 +0800

    pyttsx3讲中文

--- tts.py
+++ tts.py
@@ -1,6 +1,10 @@
 #coding:utf-8
-import pyttsx
-engine = pyttsx.init()
-engine.say('Hello World')
-engine.say('富强，民主，文明，和谐')
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty("voice","zh")
+engine.say('韩子昂 高级驾驶员，北京第三区交通委提醒您：道路千万条，安全第一条。行车不规范，亲人两行泪!')
+voices=engine.getProperty("voices")
+#for item in voices:
+#    print(item)
+engine.say('富强，民主，文明，和谐！')
 engine.runAndWait()

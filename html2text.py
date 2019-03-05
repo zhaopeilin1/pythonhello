7c3f1657b5fe0e70687352802bfeac2590d7bc12
zpl <zpl@hotmail.com>
2019-03-05 11:35:59 +0800

    html转文字，增加html文件转换功能

--- html2text.py
+++ html2text.py
@@ -1,7 +1,12 @@
 #coding:utf-8
+#import os
+# 使用goose3 把html新闻网页html转化为文本，不含图片等多媒体。 可以用url 和 离线html文件
 from goose3 import Goose
 from goose3.text import StopWordsChinese
-url  = 'http://www.bbc.co.uk/zhongwen/simp/chinese_news/2012/12/121210_hongkong_politics.shtml'
+url  = 'http://world.people.com.cn/n1/2019/0305/c1002-30957989.html'
+#html_file = 'c1002-30957989.html'
 g = Goose({'stopwords_class': StopWordsChinese})
+#article = g.extract(raw_html=open(html_file).read())
 article = g.extract(url=url)
-print article.cleaned_text[:150]
+print(article.cleaned_text)
+

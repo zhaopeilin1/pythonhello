f97e2f0401ae3d199b4caf324cd5c13841b2f9d3
zpl <zpl@hotmail.com>
2019-03-09 10:58:21 +0800

    写入txt

--- html2text.py
+++ html2text.py
@@ -3,6 +3,10 @@
 # 使用goose3 把html新闻网页html转化为文本，不含图片等多媒体。 可以用url 和 离线html文件
 from goose3 import Goose
 from goose3.text import StopWordsChinese
+
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
 url  = 'http://world.people.com.cn/n1/2019/0305/c1002-30957989.html'
 #html_file = 'c1002-30957989.html'
 g = Goose({'stopwords_class': StopWordsChinese})
@@ -10,3 +14,12 @@ g = Goose({'stopwords_class': StopWordsChinese})
 article = g.extract(url=url)
 print(article.cleaned_text)
 
+#从文件中取内容
+def extractFromFile(html_file,g):
+   article = g.extract(raw_html=open(html_file).read())
+   return article.cleaned_text
+
+#从url中取内容
+def extractFromUrl(inputurl,g):
+   article = g.extract(url=inputurl)
+   return article.cleaned_text
